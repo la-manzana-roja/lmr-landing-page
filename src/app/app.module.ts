938ca3b7a508ab59c3822eb644ed31a9758ae4ae
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-03-16 00:31:34 -0400

    Initial banner design implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, HomeModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
