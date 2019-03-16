3b9e30a36d08b3337940909c1797be3736a1836a
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-03-16 18:35:23 -0400

    Solve imports order

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidenavDirective } from './navbar/sidenav/sidenav.directive';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent, SidenavDirective],
