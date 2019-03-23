0dd1d8a0411c1f8076bf277e3d699abab8ca5408
mariagarciaflores <grissgarcia74@gmail.com>
2019-03-23 17:52:03 -0400

    Metric section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from './banner/banner.component';
+import { MetricsComponent } from './metrics/metrics.component';
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent],
-  imports: [CommonModule, SharedModule]
+  declarations: [HomeComponent, BannerComponent, MetricsComponent],
+  imports: [CommonModule, FormsModule, SharedModule]
 })
 export class HomeModule {}
---
+++ src/app/home/metrics/metrics.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lmr-metrics',
+  templateUrl: './metrics.component.html',
+  styleUrls: ['./metrics.component.scss']
+})
+export class MetricsComponent implements OnInit {
+  foodMetric = '0';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  animateMetric() : void {
+    
+  }
+
+}
