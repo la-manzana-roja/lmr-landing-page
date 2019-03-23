30fbd09390a553f19b542d8ad833f40734e247c0
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-03-23 02:01:23 -0400

    News component was implemented

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { CloudFunctions } from '../shared/data/cloud-functions';
+import { New } from '../shared/models/new';
 import { NewsService } from '../shared/news/news.service';
 
 @Component({
@@ -8,11 +12,21 @@ import { NewsService } from '../shared/news/news.service';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  news$: Observable<New[]>;
+  topNews$: Observable<New[]>;
   defaultImage: string;
 
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
+    this.news$ = this.newsService.getAllNews();
+    this.topNews$ = this.newsService.getTopNews();
     this.defaultImage = this.newsService.defaultNewImage;
+  }
+
+  updateNewViews(link: New): void {
+    this.newsService
+      .updateUsingCloudFunction(link.id, CloudFunctions.updateNewViews)
+      .subscribe();
   }
 }
